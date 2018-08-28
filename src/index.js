c88067151e042c15970cae3c08c1c7f85a3c9c42
__strelkov <stkdmitry@ya.ru>
2018-08-28 14:07:59 +0300

    media publick method

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import { ThemeProvider } from 'styled-components';
 import Col from './Col';
 import Row from './Row';
 import Grid from './Grid';
+import { media } from './helpers';
 
 export {
   Col,
   Row,
   Grid,
   ThemeProvider,
+  media,
 };
